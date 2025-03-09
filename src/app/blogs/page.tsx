2e5e5b941ef0fe2a04608634ca3364a07cd3d27e
SMNoushan <noushan.md@gmail.com>
2025-03-10 01:45:21 +0600

    configured: redux; update: blogs => fetch using rtk query

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,15 +1,19 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
-import { Metadata } from "next";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blogs.slice";
+// import { Metadata } from "next";
 import { Blog } from "@/types";
 
-export const metadata: Metadata = {
-  title: "NexaBlog | Blogs",
-  description: "Explore all blogs on NexaBlog",
-};
+// export const metadata: Metadata = {
+//   title: "NexaBlog | Blogs",
+//   description: "Explore all blogs on NexaBlog",
+// };
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", { cache: "no-store" });
-  const blogs = await res.json();
+const BlogsPage = () => {
+  const { data: blogs, isLoading } = useGetBlogsQuery(undefined);
+  // const res = await fetch("http://localhost:5000/blogs", { cache: "no-store" });
+  // const blogs = await res.json();
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-3xl text-center my-5 font-bold">
@@ -21,11 +25,15 @@ const BlogsPage = async () => {
           unprecedented computational power.
         </i>
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-        {blogs.map((blog: Blog) => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+          {blogs.map((blog: Blog) => (
+            <BlogCard key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
