5bb84d6238715cc01837c19c9eb348e6c0bbcef7
SMNoushan <noushan.md@gmail.com>
2025-03-09 17:45:51 +0600

    add: ssg for first 3 blogs

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,5 +1,15 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
 
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: { revalidate: 900 },
+  });
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: { id: string }) => ({
+    blogId: blog.id,
+  }));
+};
+
 const BlogDetailsPage = async ({
   params,
 }: {
