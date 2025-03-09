bc54ed17c4cf216e2235ce0566e3fc7bcab919c4
SMNoushan <noushan.md@gmail.com>
2025-03-10 00:53:46 +0600

    add: dynamic metadata

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -10,15 +10,28 @@ export const generateStaticParams = async () => {
   }));
 };
 
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+
+  return {
+    title: `NexaBlog | ${blog.title}`,
+    description: blog.description,
+  };
+};
+
 const BlogDetailsPage = async ({
   params,
 }: {
   params: Promise<{ blogId: string }>;
 }) => {
   const { blogId } = await params;
-  const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
   const blog = await res.json();
 
   return <BlogDetailsCard blog={blog} />;
